f1dbda1ac5116592574a3061b5f4a0ab2334478b
Yan Lyra <yanlyra3@gmail.com>
2021-10-24 21:06:44 -0300

    🌱 Created a seed to create random users in dev env

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from "miragejs";
+import faker from "faker";
+import { createServer, Factory, Model } from "miragejs";
 
 type User = {
   name: string;
@@ -11,6 +12,24 @@ export function makeServer() {
     environment: "test",
     models: {
       user: Model.extend<Partial<User>>({}),
+    },
+
+    factories: {
+      user: Factory.extend({
+        name() {
+          return faker.name.findName();
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10).toISOString();
+        },
+      }),
+    },
+
+    seeds(server) {
+      server.createList("user", 200);
     },
 
     routes() {
